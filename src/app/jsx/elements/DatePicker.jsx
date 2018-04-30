cba6274c2e5b05b6959ec5972b35d54627ccdccf
lgcolella <luigicolella.1997@gmail.com>
2018-04-30 08:58:46 +0200

    Aggiunta localizzazione italiana per il datepicker.

--- src/app/jsx/elements/DatePicker.jsx
+++ src/app/jsx/elements/DatePicker.jsx
@@ -1,5 +1,58 @@
 import React from 'react';
 
+const i18nOptions = {
+    cancel: 'Annulla',
+    clear: 'Pulisci',
+    done: 'Ok',
+    months: [
+        'Gennaio',
+        'Febbraio',
+        'Marzo',
+        'Aprile',
+        'Maggio',
+        'Giugno',
+        'Luglio',
+        'Agosto',
+        'Settembre',
+        'Ottobre',
+        'Novembre',
+        'Dicembre'
+    ],
+    monthsShort: [
+        'Gen',
+        'Feb',
+        'Mar',
+        'Apr',
+        'Mag',
+        'Giu',
+        'Lug',
+        'Ago',
+        'Set',
+        'Ott',
+        'Nov',
+        'Dic'
+    ],
+    weekdays: [
+        'Domenica',
+        'Lunedì',
+        'Martedì',
+        'Mercoledì',
+        'Giovedì',
+        'Venerdì',
+        'Sabato'
+    ],
+    weekdaysShort: [
+        'Dom',
+        'Lun',
+        'Mar',
+        'Mer',
+        'Gio',
+        'Ven',
+        'Sab'
+    ],
+    weekdaysAbbrev: ['D','L','M','M','G','V','S']
+}
+
 export default class DatePicker extends React.Component {
 
     constructor(props){
@@ -15,10 +68,7 @@ export default class DatePicker extends React.Component {
         var options = {
             format: 'yyyy-mm-dd',
             showClearBtn: true,
-            i18n: {
-                'cancel': 'Cancella',
-                'clear': 'Pulisci'
-            },
+            i18n: i18nOptions,
         };
         if (typeof this.props.onChange !== 'undefined'){
             options['onClose'] = () => {this.props.onChange()};
