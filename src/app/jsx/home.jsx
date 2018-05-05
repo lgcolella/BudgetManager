e5e09ddc4c0aa3d9523cf6f1f5f640d5dcb07298
lgcolella <luigicolella.1997@gmail.com>
2018-05-05 17:12:55 +0200

    Incapsulato componente tabella principale e fix generici.

--- src/app/jsx/home.jsx
+++ src/app/jsx/home.jsx
@@ -1,12 +1,11 @@
 import Storage from '../storage.js';
 import Utils from './functions/utils.js';
 import React from 'react';
+import TableOverview from './components/TableOverview.jsx';
 import SideNav from './components/SideNav.jsx';
 import FiltersMenu from './components/FiltersMenu.jsx';
 import EditActivity from './components/EditActivity.jsx';
-import Pagination from './elements/Pagination.jsx';
 import FormSelect from './elements/FormSelect.jsx';
-import { throttle } from 'rxjs/operator/throttle';
 
 const sideNavId = 'sidenav';
 const modalNewActivityId = 'new-activity-modal';
@@ -27,18 +26,15 @@ export default class Home extends React.Component {
             'dataToRender': data,
             'activityToEdit': undefined,
             'filters': defaultStorage.getData().filters,
-            'elementsInPaginationGroup': 5
         };
         this.importData = this.importData.bind(this);
         this.addActivity = this.addActivity.bind(this);
         this.editActivity = this.editActivity.bind(this);
-        this.deleteActivity = this.deleteActivity.bind(this);
         this.addFilters = this.addFilters.bind(this);
         this.clearFilters = this.clearFilters.bind(this);
         this.filterData = this.filterData.bind(this);
-        this.sortColumns = this.sortColumns.bind(this);
-        this.checkActivity = this.checkActivity.bind(this);
-        this.changeDisplayedElements = this.changeDisplayedElements.bind(this);
+        this.setStateData = this.setStateData.bind(this);
+        this.setActivityToEdit = this.setActivityToEdit.bind(this);
     }
 
     importData(data){
@@ -79,24 +75,6 @@ export default class Home extends React.Component {
             data: newData,
             dataToRender: this.state.data.map(mapCb),
         });
-        defaultStorage.setData({
-            data: newData,
-            filters: this.state.filters
-        });
-    }
-
-    deleteActivity(selectedActivity){
-
-        function filterCb(object){
-            return ( selectedActivity.id === object.id ? false : true );
-        }
-
-        var newData = this.state.data.filter(filterCb);
-        this.setState({
-            data: newData,
-            dataToRender: this.state.dataToRender.filter(filterCb)
-        });
-
         defaultStorage.setData({
             data: newData,
             filters: this.state.filters
@@ -228,158 +206,20 @@ export default class Home extends React.Component {
         return dataToRender;
     }
 
-    sortColumns(event, dataToSort){
-
-        const aLessThanB = (function toggleIconAndSetTypeOfSort(){
-            var el = event.target;
-            var elSimilars = document.querySelectorAll('#table-overview thead th i');
-            var newIcon;
-            var aMinusB;
-            if ( el.innerText === 'sort' || el.innerText === 'arrow_drop_up' ){
-                newIcon = 'arrow_drop_down';
-                aMinusB = -1;
-            } else if ( el.innerText === 'arrow_drop_down' ){
-                newIcon = 'arrow_drop_up';
-                aMinusB = 1;
-            }
-            Array.from(elSimilars).forEach(function(i){
-                if (i === el){
-                    el.innerText = newIcon;
-                } else {
-                    i.innerText = 'sort';
-                }
-            });
-            return aMinusB;
-        })();
-
-        var columnIndex = (() => {
-            var el = event.target;
-            while (el.nodeName !== 'TH'){
-                el = el.parentNode;
-            }
-            var index = 0;
-            while (el.previousSibling !== null){
-                el = el.previousSibling;
-                index += 1;
-            }
-            return index;
-        })();
-        
-        var valuesToSort = ((columnIndex) => {
-            var values = [];
-            var tableRows = document.querySelectorAll('#table-overview>tbody>tr');
-            Array.from(tableRows).forEach(function(tr){
-                Array.from(tr.childNodes).forEach(function(td, index){
-                    if (index === columnIndex){
-                        var value = ( td.innerText.indexOf('€') !== -1 ? Number(td.innerText.replace('€', '')) : td.innerText );
-                        values.push(value);
-                    }
-                });
-            });
-            return values;
-        })(columnIndex);
-
-        var sortedValues = valuesToSort.sort(function(a, b){
-            a = (typeof a === 'string' ? a.toLowerCase() : a);
-            b = (typeof a === 'string' ? b.toLowerCase() : b);
-            if (a < b){ return aLessThanB }
-            else if (a > b){ return -aLessThanB }
-            else { return 0 }
-        });
-
-        var sortedDataToRender = this.state.dataToRender.sort(function(obj1, obj2){
-
-            var prop1 = obj1[dataToSort];
-            var prop2 = obj2[dataToSort];
-            if ( sortedValues.indexOf(prop1) < sortedValues.indexOf(prop2) ){ return -1 }
-            else if ( sortedValues.indexOf(prop1) > sortedValues.indexOf(prop2) ){ return 1 }
-            else { return 0 }
-
-        });
-
-        this.setState({
-            dataToRender: sortedDataToRender
-        });
-
-    }
-
-    checkActivity(event){
-        var data = this.state.data;
-        var inputValue = event.target.value;
-
-        if (inputValue === 'all'){
-            data = data.map((activity) => {
-                activity['selected'] = event.target.checked;
-                return activity;
-            });
-        } else {
-            var id = Number(inputValue);
-            data = data.map((activity) => {
-                if (activity.id === id){
-                    activity['selected'] = event.target.checked;
-                }
-                return activity;
-            });
-        }
-        
+    setStateData(data){
         this.setState({
             data
         });
+        defaultStorage.setData({
+            data,
+            filters: this.state.filters
+        });
     }
 
-    changeDisplayedElements(groupNum){
-
-        if (groupNum === 'all'){
-            var hiddenRows = document.querySelectorAll('#'+tableOverviewId+'>tbody>tr.hide');
-            if (hiddenRows !== null);
-            hiddenRows.forEach((row) => {
-                row.classList.remove('hide');
-            });
-        } else {
-            var elementsInGroup = this.state.elementsInPaginationGroup;
-            var rows = document.querySelectorAll('#'+tableOverviewId+'>tbody>tr');
-            rows.forEach((row, index) => {
-                if (groupNum - 1 <= index/elementsInGroup && index/elementsInGroup < groupNum){
-                    row.classList.remove('hide');
-                } else {
-                    row.classList.add('hide');
-                }
-            });
-        }
-
-    }
-
-    changePaginationGroup(event){
-        var value = event.target.value;
-        if (value > 0){
-            this.setState({
-                elementsInPaginationGroup: value
-            }, () => {
-                var activeButton = document.querySelector('#table-overview-pagination li.active');
-                if (activeButton !== null){
-                    activeButton.classList.remove('active');
-                }
-                document.querySelectorAll('#table-overview-pagination li')[1].classList.add('active');
-                this.changeDisplayedElements(1);
-            });
-        } else {
-            this.setState({
-                elementsInPaginationGroup: ''
-            }, () => {
-                this.changeDisplayedElements('all');
-            });
-        }
-    }
-
-    componentDidMount(){
-        var elem = document.querySelectorAll('.tooltipped');
-        M.Tooltip.init(elem);
-        this.changeDisplayedElements(1);
-    }
-
-    componentDidUpdate(){
-        var elem = document.querySelectorAll('.tooltipped');
-        M.Tooltip.init(elem);
+    setActivityToEdit(activityToEdit){
+        this.setState({
+            activityToEdit
+        });
     }
 
     render() {
@@ -387,38 +227,6 @@ export default class Home extends React.Component {
         var dataToRender = this.filterData();
         var dataInfo = Utils.getDataInfo(data, dataToRender);
 
-        let tbodyHTML = dataToRender.map((object) => {
-
-            var type = (Number(object.amount) > 0 ? <i className='material-icons green-text'>arrow_upward</i> : <i className='material-icons red-text'>arrow_downward</i> );
-            var selected = (typeof object.selected === 'undefined' ? true : object.selected );
-
-            return (
-                <tr key={object.id}>
-                    <td>{type}</td>
-                    <td>
-                        <label>
-                            <input type="checkbox" value={object.id} checked={selected} onChange={(event) => this.checkActivity(event)}></input>
-                            <span></span>
-                        </label>
-                    </td>
-                    <td>{object.wallet}</td>
-                    <td>{object.activity}</td>
-                    <td><i>{object.amount.toString() + '€'}</i></td>
-                    <td><i>{object.date}</i></td>
-                    {/*<td>{object.fromDate}</td>
-                    <td>{object.toDate}</td>*/}
-                    <td>
-                        <div>
-                            <a href='#!'><i className='material-icons tooltipped' data-position="top" data-tooltip={object.comment}>comment</i></a>
-                            <a href='#!'><i className='material-icons modal-trigger' data-target={editActivityModalId}
-                                onClick={() => {this.setState({activityToEdit: object})}}>edit</i>
-                            </a>
-                            <a href='#!'><i className='material-icons hover-red' onClick={() => this.deleteActivity(object)}>delete</i></a>
-                        </div>
-                    </td>
-                </tr>
-            );
-        });
 
         return (
             <div>
@@ -489,50 +297,15 @@ export default class Home extends React.Component {
                         ></FiltersMenu>
                     </div>
                     <div className='col s9'>
-                        <div id='pagination-wrapper' className='row'>
-                            <div id='pagination-choice' className='col s3'>
-                                <label>Risultati per pagina</label>
-                                <input type='number' value={this.state.elementsInPaginationGroup} onChange={(event) => this.changePaginationGroup(event)}></input> 
-                            </div>
-                            <div className='col s9'>
-                                <Pagination
-                                elementsInGroup={this.state.elementsInPaginationGroup}
-                                elementsNum={dataToRender.length}
-                                onChange={this.changeDisplayedElements}
-                                ></Pagination>
-                            </div>
-                        </div>
-                        <div className='row'>
-                            <div className='col s12'>
-                                <table id={tableOverviewId} className='highlight centered'>
-                                    <thead>
-                                    <tr>
-                                        <th></th>
-                                        <th>
-                                            <div>
-                                                <label>
-                                                    <input type="checkbox" value={'all'} defaultChecked={true} onClick={(event) => this.checkActivity(event)}></input>
-                                                    <span></span>
-                                                </label>
-                                            </div>
-                                        </th>
-                                        <th> Portafoglio <i className='material-icons' onClick={(event) => this.sortColumns(event, 'wallet')}>sort</i></th>
-                                        <th>Attività <i className='material-icons' onClick={(event) => this.sortColumns(event, 'activity')}>sort</i></th>
-                                        <th>Importo <i className='material-icons' onClick={(event) => this.sortColumns(event, 'amount')}>sort</i></th>
-                                        <th>Data <i className='material-icons' onClick={(event) => this.sortColumns(event, 'date')}>sort</i></th>
-                                        <td>
-                                            <div>
-                                                <i className='material-icons' onClick={() => {M.Modal.getInstance(document.getElementById(modalNewActivityId)).open();}}>add_shopping_cart</i>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                        {tbodyHTML}
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                        <TableOverview
+                        id={tableOverviewId}
+                        data={data}
+                        dataToRender={dataToRender}
+                        modalEditActivityId={editActivityModalId}
+                        modalNewActivityId={modalNewActivityId}
+                        onChangeData={this.setStateData}
+                        onChangeActivityToEdit={this.setActivityToEdit}
+                        ></TableOverview>
                     </div>
                     
                     <EditActivity
